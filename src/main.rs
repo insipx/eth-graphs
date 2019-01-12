51700f1e1e5c00143c97f1dc2ba4396b5b284028
Andrew Plaza <aplaza@liquidthink.net>
2019-01-11 19:03:25 -0500

    original algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod graphs;
 
-pub const DATA_LENGTH: usize = 3;
+pub const DATA_LENGTH: usize = 200;
 
 pub enum XAxis {
     Gas,
@@ -51,10 +51,10 @@ impl PlotType {
     // returns a range to correspond what x/y axes should be
     pub fn range(&self) -> ((f64, f64), (f64, f64)) {
         match self {
-            PlotType::ConstantGasPriceNoBoost => ((0.0, 1_500_000.00), (20_950.0, 21_060.00)),
-            PlotType::ConstantGasNoBoost => ((0.0, 1_00_000_000.0), (0.0, 110_000_000.00)),
-            PlotType::ConstantGasPrice => ((0.0, 25_000.00), (19_000.00, 22_000.00)),
-            PlotType::ConstantGas => ((0.0, 1_500_000.0), (0.0, 70_000_000.0)),
+            PlotType::ConstantGasPriceNoBoost => ((0.0, 21_000_000.00), (20_900.0, 21_200.00)),
+            PlotType::ConstantGasNoBoost => ((0.0, 200_000.0), (0.0, 200_000.00)),
+            PlotType::ConstantGasPrice => ((0.0, 200_000.00), (685_000_000.0, 696_000_000.0)),
+            PlotType::ConstantGas => ((0.0, 200_000.0), (0.0, 6_500_000_000.0)),
             PlotType::LinearGasPrice => ((0.0, 22_000.0), (0.0, 600_000_000.0)),
             PlotType::LinearGasPriceNoBoost => ((0.0, 1_0_000.0), (0.0, 420_000.0)),        }
     }
@@ -63,13 +63,13 @@ impl PlotType {
 fn main() {
 
     let gas_price_multiplier = |i:usize| {
-        (10u64.pow((i * 3) as u32)) as f64
+        (i * 1000) as f64
     };
     // constant gas
-    graphs::score_graph(|_| 21_000.0, gas_price_multiplier, |score| score, XAxis::GasPrice, PlotType::ConstantGasNoBoost);
-    graphs::score_graph(|_| 21_000.0, gas_price_multiplier, |score| ((score as u64) << 15) as f64, XAxis::GasPrice, PlotType::ConstantGas);
-    // graphs::score_graph(|i| ((10_000.0) * (10.0 * i as f64)), |_| 21_000.0, |score| score, XAxis::Gas, PlotType::ConstantGasPriceNoBoost); // GOOD
-    // graphs::score_graph(|i| ((100.0) * (10.0 * i as f64)), |_| 21_000.0, |score| ((score as u64) << 15) as f64, XAxis::Gas, PlotType::ConstantGasPrice);// GOOD
+    // graphs::score_graph(|_| 21_000.0, gas_price_multiplier, |score| score, XAxis::GasPrice, PlotType::ConstantGasNoBoost);
+    // graphs::score_graph(|_| 21_000.0, gas_price_multiplier, |score| ((score as u64) << 15) as f64, XAxis::GasPrice, PlotType::ConstantGas);
+    graphs::score_graph(|i| ((10_000.0) * (10.0 * i as f64)), |_| 21_000.0, |score| score, XAxis::Gas, PlotType::ConstantGasPriceNoBoost); // GOOD
+    graphs::score_graph(|i| ((100.0) * (10.0 * i as f64)), |_| 21_000.0, |score| ((score as u64) << 15) as f64, XAxis::Gas, PlotType::ConstantGasPrice);// GOOD
 }
 
 fn gen_original_algorithm_plots() {
