044eec453c9be433d576e4678ddc877bfd19822e
Andrew Plaza <aplaza@liquidthink.net>
2019-01-29 19:46:09 -0500

    add graph with longer X axis for gas graph

--- src/main.rs
+++ src/main.rs
@@ -18,38 +18,6 @@ pub enum PlotType {
     ConstantGas,
 }
 
-impl PlotType {
-
-    pub fn axis(&self) -> &str {
-        match self {
-            PlotType::ConstantGasPrice => "Gas",
-            PlotType::ConstantGas => "Gas Price",
-        }
-    }
-
-    pub fn name(&self) -> &str {
-        match self {
-            PlotType::ConstantGasPrice => "Constant Gas Price",
-            PlotType::ConstantGas => "Constant Gas",
-        }
-    }
-
-    pub fn file_name(&self) -> &str {
-        match self {
-            PlotType::ConstantGasPrice => "ConstantGasPrice.svg",
-            PlotType::ConstantGas => "ConstantGas.svg",
-        }
-    }
-
-    // returns a range to correspond what x/y axes should be
-    pub fn range(&self) -> ((f64, f64), (f64, f64)) {
-        match self {
-            PlotType::ConstantGasPrice => ((0.0, 200_000.00), (685_000_000.0, 696_000_000.0)),
-            PlotType::ConstantGas => ((0.0, 200_000.0), (0.0, 6_500_000_000.0)),
-        }
-    }
-}
-
 fn main() {
 
     let gas_price_multiplier = |i:usize| {
